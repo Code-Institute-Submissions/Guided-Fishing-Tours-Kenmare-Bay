fefccb40411911a0a356a7abc730df9d0c1d32fe
alchemist2016 <vukdragovic@hotmail.com>
2020-01-20 09:51:26 +0000

    Added a back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,24 @@ function initMap() {
     var markerCluster = new MarkerClusterer(map, markers,
         { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
+
+/*-----------------------Back to top button---------*/
+//Get the button element
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
