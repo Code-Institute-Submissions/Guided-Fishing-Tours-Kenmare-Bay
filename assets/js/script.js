7f8f039865c54ee46f4c2251f235ec86d759caed
alchemist2016 <vukdragovic@hotmail.com>
2020-02-02 21:55:22 +0000

    Creating search bar for google map and debbuging it

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,98 +137,30 @@ function initMap() {
             });
         }
     }
+    /*---------------------------Search bar for google map ------------------*/
 
+    var input = document.getElementById('search');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var locations = [];
+
+    searchBox.addListener('places_changed', function(){
+        var places = searchBox.getPlaces();
+    });
+
+    if (places.length === 0){
+        return;
+    }
+
+    locations.forEach(function(m) { m.setMap(null)});
+    locations = [];
 }
 
 
-
-// function initMap() {
-//     var map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 10,
-//         center: centerCoords,
-//         title: 'Kenmare'
-//     });
-//     addMarkerInfo();
-// }
-
-//     function addMarkerInfo(){
-//         for(var i = 0; i < markersOnMap.length; i++){
-//             var contentString = '<h3>' + markersOnMap[i].placeName + '</h3>';
-//             const marker = new google.maps.Marker({
-//                 position: markersOnMap[i].LatLng[0],
-//                 // icon: icons[markersOnMap[i].type].icon,
-//                 map: map
-//             });
-//             const infowindow = new google.maps.InfoWindow({
-//                 content: contentString
-//             });
-//             marker.addListener('click', function() {
-//                 closeOtherInfo();
-//                 infowindow.open(map, marker);
-//                 InfoObj[0] = infowindow;
-//             });
-//         }
-//     }
-
-//     function closeOtherInfo() {
-//         if(InfoObj.length > 0) {
-//             InfoObj[0].set("marker", null);
-//             InfoObj[0].close();
-//             InfoObj[0].length = 0;
-//         } 
-//     }
-
-
-
-// var labels = ["Kenmare", "Blackwater Harbour", "Oysterbed Pier", "Gleesk Harbour", "Lamb's Head", "Derrynane"];
-
-// var markers = [
-//     ["Kenmare", 51.880097, -9.583517],
-//     ["BlackWater Harbour", 51.8501559, -9.7464887],
-//     ["Oysterbed Pier", 51.8125, -9.8963],
-//     ["Gleesk Harbour", 51.8012, -9.9372],
-//     ["Lamb's Head", 51.7452778, -10.1327778],
-//     ["Derrynane", 51.7595812, -10.1254581]
-// ];
-
-
-// var contentString = [
-//     ['<div class="info_content">' +
-//         '<h3>Kenmare</h3>' +
-//         '<p>Lorem Ipsum  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>' + '</div>'],
-//     ['<div class="info_content">' +
-//         '<h3>BlackWater Harbour</h3>' +
-//         '<p>Lorem Ipsum  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>' + '</div>'],
-//     ['<div class="info_content">' +
-//         '<h3>Oysterbed Pier</h3>' +
-//         '<p>Lorem Ipsum  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>' + '</div>'],
-//     ['<div class="info_content">' +
-//         '<h3>Gleesk Harbour</h3>' +
-//         '<p>Lorem Ipsum  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>' + '</div>'],
-//     ['<div class="info_content">' +
-//         "<h3>Lamb's Head</h3>" +
-//         '<p>Lorem Ipsum  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>' + '</div>'],
-//     ['<div class="info_content">' +
-//         "<h3>Derrynane</h3>" +
-//         '<p>Lorem Ipsum  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</p>' + '</div>']
-// ];
-
-
-// var infowindow = new google.maps.InfoWindow(), marker, i;
-
-// for (var i = 0; i < markers.length; i++) {
-//     var marker = new google.maps.Marker({
-//         position: markers[i][1], markers[i][2],
-//         map: map,
-//         title: markers[i][0];
-//     });
-//     marker.addListener('click', function () {
-//         infowindow.open(map, marker);
-//     });
-// }
-
-// var markerCluster = new MarkerClusterer(map, markersOnMap,
-//     { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
 
 
@@ -255,7 +187,7 @@ function topFunction() {
 
 /*---------------Submit button in Contact Form----------------*/
 
-function btnFunction(){
+function btnFunction() {
     alert("Your message has been sent!");
     location.reload();
 }
