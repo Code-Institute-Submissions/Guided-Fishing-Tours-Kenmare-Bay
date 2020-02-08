494749d9b4d03e2a0d55b824b9162e9faa108c2f
alchemist2016 <vukdragovic@hotmail.com>
2020-02-08 13:34:50 +0000

    sendEmail.js file validated

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,16 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "guided_fishing_tours", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "tour_request": contactForm.tour_request.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+	emailjs.send("gmail", "guided_fishing_tours", {
+			"from_name": contactForm.name.value,
+			"from_email": contactForm.email.value,
+			"tour_request": contactForm.tour_request.value
+		})
+		.then(
+			function(response) {
+				console.log("SUCCESS", response);
+			},
+			function(error) {
+				console.log("FAILED", error);
+			}
+		);
+	return false; // To block from loading a new page
 }
-    
-
